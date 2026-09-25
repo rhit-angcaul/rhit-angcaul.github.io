e88d9ae50cc50c04e1407d2863064dad82dd6e5e
rhit-angcaul <caulfias@rose-hulman.edu>
2026-09-25 00:57:09 -0400

    can this work

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ let linkBtn = document.getElementsByName("linkBtn")
 // https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page
 
 resumeBtn.onclick = function(){
-    window.location.href="/resume.html"
+    window.location.href="https://rhit-angcaul.github.io/resume.html"
 }
