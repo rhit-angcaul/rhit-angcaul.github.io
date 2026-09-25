7844dd404054c11bc433763420dbe557f9ca236b
rhit-angcaul <caulfias@rose-hulman.edu>
2026-09-25 01:10:46 -0400

    ???

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 
-let resumeBtn = document.querySelector('[name="resumeBtn"]')
-let portBtn = document.getElementsByName("portBtn")
-let handBtn = document.getElementsByName("handBtn")
-let linkBtn = document.getElementsByName("linkBtn")
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+
+function DOMcontentLoaded(){
+    let resumeBtn = document.querySelector('[name="resumeBtn"]')
+    let portBtn = document.querySelector('[name="portBtn"]')
+    let handBtn = document.querySelector('[name="handBtn"]')
+    let linkBtn = document.querySelector('[name="linkBtn"]')
 
 
 // https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page
 
-resumeBtn.addEventListener("click", function(){
+    resumeBtn.addEventListener("click", function(){
     window.location.href="https://rhit-angcaul.github.io/resume.html"
-})
+    })
+}
